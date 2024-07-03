680558b71a72b6ede7336503cc3fc87ae4fbdcb4
erik.minasian <erik.minasian@coliquio.de>
2024-07-03 16:39:40 +0400

    feat: image display added

---
+++ app/news/[id]/image/page.js
@@ -0,0 +1,13 @@
+import { getAllNews } from "@/utils/news";
+import Image from "next/image";
+
+export default function NewsImagePage({ params }) {
+  const news = getAllNews();
+  const item = news.find(({ slug }) => slug === params.id);
+
+  return (
+    <div className="image-container">
+      <Image src={`/images/news/${item.image}`} fill />
+    </div>
+  );
+}
--- app/news/[id]/page.js
+++ app/news/[id]/page.js
@@ -1,14 +1,21 @@
 import { DUMMY_NEWS } from "@/utils/dummy-news";
 import { notFound } from "next/navigation";
+import Link from "next/link";
 
 export default function NewsItemPage({ params }) {
-
-  const news = DUMMY_NEWS.find(item => item.slug === params?.id)
+  const news = DUMMY_NEWS.find((item) => item.slug === params?.id);
 
   if (!news) {
-    notFound()
+    notFound();
   }
 
+  return (
+    <>
+      <h2>News Item: {news?.title}</h2>
 
-  return <h2>News Item: {news?.title}</h2>;
+      <Link href={`/news/${news.slug}/image`}>
+        <span>Click here for the image.</span>
+      </Link>
+    </>
+  );
 }
