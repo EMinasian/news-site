7766564210143e3dbac968658265caefd6f2409e
erik.minasian <erik.minasian@coliquio.de>
2024-07-04 15:33:19 +0400

    feat: modal backdrop navigates back

--- app/news/[id]/(.)image/page.js
+++ app/news/[id]/(.)image/page.js
@@ -1,13 +1,22 @@
+"use client";
+
 import { getAllNews } from "@/utils/news";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 export default function NewsImagePage({ params }) {
   const news = getAllNews();
   const item = news.find(({ slug }) => slug === params.id);
+  const router = useRouter();
 
   return (
     <>
-      <div className="modal-backdrop">
+      <div
+        className="modal-backdrop"
+        onClick={() => {
+          router.back();
+        }}
+      >
         <dialoge className="modal" open>
           <div className="image-container">
             <Image src={`/images/news/${item.image}`} fill />
