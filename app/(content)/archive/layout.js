f265f044e79c2eae58d701fdf51b23954ed8db2f
erik.minasian <erik.minasian@coliquio.de>
2024-07-12 18:37:14 +0400

    feat: suspense for loading

--- app/(content)/archive/layout.js
+++ app/(content)/archive/layout.js
@@ -1,10 +1,12 @@
+import { Suspense } from "react";
+
 export default function ArchiveLatout({ archive, latest }) {
   return (
     <>
       <h2>Archive</h2>
-      {archive}
+      <Suspense fallback={<p>Loading the archive...</p>}>{archive}</Suspense>
       <h2>Latest</h2>
-      {latest}
+      <Suspense fallback={<p>Loading the latest news...</p>}>{latest}</Suspense>
     </>
   );
 }
