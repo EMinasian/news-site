b0c48f74bf05264739510e646cfb7297209f7d95
erik.minasian <erik.minasian@coliquio.de>
2024-07-15 18:00:12 +0400

    feat: suspense for archive news section

--- app/(content)/archive/@archive/[[...filter]]/page.js
+++ app/(content)/archive/@archive/[[...filter]]/page.js
@@ -3,12 +3,16 @@ import NewsList from "@/components/NewsList";
 import NavigationBar from "@/components/NavigationBar";
 import { Suspense } from "react";
 
-export default async function YearPage({ params }) {
-  const [year, month] = params?.filter ? params?.filter : [];
-
+async function News({ year, month }) {
   const news = month
     ? await getNewsForYearAndMonth(year, month)
     : await getNewsForYear(year);
+
+  return <NewsList news={news} />;
+}
+
+export default async function YearPage({ params }) {
+  const [year, month] = params?.filter ? params?.filter : [];
 
   return (
     <>
@@ -17,7 +21,9 @@ export default async function YearPage({ params }) {
           <NavigationBar year={year} />
         </Suspense>
       </header>
-      {params?.filter && <NewsList news={news} />}
+      <Suspense fallback={<p>Suspence for news section...</p>}>
+        <News year={year} month={month} />
+      </Suspense>
     </>
   );
 }
--- app/(content)/archive/layout.js
+++ app/(content)/archive/layout.js
@@ -4,7 +4,7 @@ export default function ArchiveLatout({ archive, latest }) {
   return (
     <>
       <h2>Archive</h2>
-      <Suspense fallback={<p>Loading the archive...</p>}>{archive}</Suspense>
+      {archive}
       <h2>Latest</h2>
       <Suspense fallback={<p>Loading the latest news...</p>}>{latest}</Suspense>
     </>
